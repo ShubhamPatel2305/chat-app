f43d17fe4f61803c2be8506a2cd62a442422b9d3
Shubham <shubhamcp23@gmail.com>
2023-02-08 10:01:16 +0530

    Initial

---
+++ src/redux/slices/app.js
@@ -0,0 +1,85 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// ----------------------------------------------------------------------
+
+const initialState = {
+  sideBar: {
+    open: false,
+    type: 'CONTACT', // can be CONTACT, STARRED, SHARED
+  },
+  isLoggedIn: true,
+  tab: 0, // [0, 1, 2, 3]
+  snackbar: {
+    open: null,
+    severity: null,
+    message: null,
+  },
+};
+
+const slice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    // Toggle Sidebar
+    toggleSideBar(state) {
+      state.sideBar.open = !state.sideBar.open;
+    },
+    updateSideBarType(state, action) {
+      state.sideBar.type = action.payload.type;
+    },
+    updateTab(state, action) {
+      state.tab = action.payload.tab;
+    },
+
+    openSnackBar(state, action) {
+
+      console.log(action.payload);
+      state.snackbar.open = true;
+      state.snackbar.severity = action.payload.severity;
+      state.snackbar.message = action.payload.message;
+    },
+    closeSnackBar(state) {
+      console.log('This is getting executed');
+      state.snackbar.open = false;
+      state.snackbar.message = null;
+    },
+  },
+});
+
+// Reducer
+export default slice.reducer;
+
+// ----------------------------------------------------------------------
+
+export const closeSnackBar = () => async (dispatch, getState) => {
+  dispatch(slice.actions.closeSnackBar());
+};
+
+export const showSnackbar = ({severity, message}) => async (dispatch, getState) => {
+  dispatch(
+    slice.actions.openSnackBar({
+      message,
+      severity,
+    }),
+  );
+
+  setTimeout(() => {
+    dispatch(slice.actions.closeSnackBar());
+  }, 4000);
+};
+
+export function ToggleSidebar() {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.toggleSideBar());
+  };
+}
+export function UpdateSidebarType(type) {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.updateSideBarType({ type }));
+  };
+}
+export function UpdateTab(tab) {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.updateTab({ tab }));
+  };
+}
